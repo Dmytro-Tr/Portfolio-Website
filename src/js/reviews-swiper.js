e32330aada76da10958e69c4770007659339d126
Dmtro_Tr <Di4gamegem@gmail.com>
2025-04-10 20:23:25 +0300

    reviews section

---
+++ src/js/reviews-swiper.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper('.mySwiper', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  keyboard: true,
+  slidesPerView: 2,
+  loop: true,
+  breakpoints: {
+    994: {
+      slidesPerView: 2,
+      centerInsufficientSlides: true,
+    },
+    0: {
+      slidesPerView: 1,
+    },
+  },
+});
