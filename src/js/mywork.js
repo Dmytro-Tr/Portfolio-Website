eab1317495e0ad9486dec3356bb3dd19e6195e4e
Dmtro_Tr <Di4gamegem@gmail.com>
2025-06-06 21:22:11 +0300

    mywork_content animation, shadow

--- src/js/mywork.js
+++ src/js/mywork.js
@@ -70,3 +70,18 @@ hideAllBtn.addEventListener('click', () => {
 
 // Ініціалізація
 updateView();
+
+/* Scroll to see the animation */
+const boxes = document.querySelectorAll('.mywork_content');
+
+const checkBoxes = () => {
+  const triggerBottom = (window.innerHeight / 5) * 4;
+  boxes.forEach(box => {
+    const boxTop = box.getBoundingClientRect().top;
+    if (boxTop < triggerBottom) box.classList.add('show');
+    else box.classList.remove('show');
+  });
+};
+
+window.addEventListener('scroll', checkBoxes);
+checkBoxes();
